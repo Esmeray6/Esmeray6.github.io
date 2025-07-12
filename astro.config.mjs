86d8d972b2d7e6f8f12c5f434162e584cb7937e1
Esmeray/Sky <22858781+Esmeray6@users.noreply.github.com>
2025-07-13 02:38:16 +0500

    AAAAAAAA

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://esmeray6.github.io',
+	output: "static",
 	integrations: [mdx(), sitemap()],
 	vite:
 		{ plugins: [tailwindcss()] }
